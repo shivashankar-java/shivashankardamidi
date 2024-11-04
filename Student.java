a781fd95577808809316c2fad06357435b60d2fe
shivashankar-java <shivashankar.damidi@gmail.com>
2024-11-04 21:01:45 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,16 @@
+package pkg;
+
+public class Student {
+    int rollNo;
+    String name;
+    int totalMarks;
+    int rank;
+
+    public Student(int rollNo, String name, int totalMarks) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.totalMarks = totalMarks;
+    }
+}
+
+
